562cada90c02531b802897fa2f6d47cc2159c2a6
= <ribhavaggarwal29@gmail.com>
2023-11-28 16:50:48 +0530

    inital commit

---
+++ backend/controllers/expense.js
@@ -0,0 +1,51 @@
+const ExpenseSchema = require('../models/ExpenseModel')
+
+exports.addExpense = async (req,res)=>{
+    const {title,amount,category,description,date} = req.body
+
+    const income = ExpenseSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+
+    try {
+        //validations
+        if(!title||!category||!description||!date){
+            return res.status(400).json({message:'All fields are required!'})
+        }
+        if(amount<=0 || !amount==='number'){//isNan??
+            return res.status(400).json({message:'Amount must be a positive number!'})
+        }
+        await income.save()
+        res.status(200).json({message:'Expense added'})
+    } catch (error) {
+        res.status(500).json({message:'Server error'})
+    }
+
+    console.log(income)
+}
+
+exports.getExpense = async(req,res)=>{
+    try {
+        const expenses = await ExpenseSchema.find().sort({createdAt:-1})
+        res.status(200).json(expenses)
+    } catch (error) {
+        res.status(500).json({message:'Server Error'})
+    }
+}
+
+
+exports.deleteExpense = async(req,res)=>{
+    const {id} = req.params;
+    //console.log(req.params)
+    ExpenseSchema.findByIdAndDelete(id)
+        .then((income)=>{
+            res.status(200).json({message:'Expense Deleted'})
+        })
+        .catch((err)=>{
+            res.status(500).json({message:'Server Error'})
+        })
+}
---
+++ backend/routes/transaction.js
@@ -0,0 +1,17 @@
+const{addIncome,getIncome,deleteIncome} = require('../controllers/income');
+const{addExpense,getExpense,deleteExpense} = require('../controllers/expense');
+const router = require('express').Router();
+
+router.post('/add-income', addIncome)
+        .get('/get-income',getIncome)
+        .delete('/delete-income/:id',deleteIncome)
+        .post('/add-expense', addExpense)
+        .get('/get-expense', getExpense)
+        .delete('/delete-expense/:id',deleteExpense)
+/*router.get('/',(req, res) => {
+    res.send('Hello World')
+})*/
+
+
+
+module.exports = router
